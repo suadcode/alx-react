dc56ce4c7c92224480201cd3076d21e18d89fa7b
suadcode <suadsalam7@gmail.com>
2024-11-06 20:08:45 +0000

    React_component

---
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './App.css';
+import PropTypes from 'prop-types';
+import CourseList from '../CourseList/CourseList.js';
+import Notifications from '../Notifications/Notifications.js';
+import Login from '../Login/Login.js';
+import Header from '../Header/Header.js';
+import Footer from '../Footer/Footer.js';
+
+
+class App extends Component {
+  static propTypes = {
+    isLoggedIn: PropTypes.bool
+  };
+
+  static defaultProps = {
+    isLoggedIn: false
+  };
+
+  constructor(props) {
+
+    super(props);
+    this.state = {
+      listCourses: [
+        { id: 1, name: 'ES6', credit: 60 },
+        { id: 2, name: 'Webpack', credit: 20 },
+        { id: 3, name: 'React', credit: 40 },
+      ],
+      listNotifications: [
+        { id: 1, type: 'default', value: 'New course available' },
+        { id: 2, type: 'urgent', value: 'New resume available' },
+        { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } },
+      ]
+    };
+  }
+
+  render() {
+    const { isLoggedIn } = this.props;
+    const { listCourses, listNotifications } = this.state;
+
+    return (
+      <div className="App">
+        <Notifications displayDrawer={true} listNotifications={listNotifications} />
+        <Header />
+        {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
