e1eac3cc835f6a7f86306e5aca83a4d39356bc99
suadcode <suadsalam7@gmail.com>
2024-11-28 15:16:34 +0000

    task_2

---
+++ 0x07-react_redux_action_creator_normalizr/task_2/dashboard/src/schema/notifications.js
@@ -0,0 +1,29 @@
+import { normalize, schema } from 'normalizr';
+import notifications from '../../../../notifications.json';
+
+
+const user = new schema.Entity('users');
+const message = new schema.Entity('messages', {}, { idAttribute: 'guid' });
+const notification = new schema.Entity('notifications', {
+  author: user,
+  context: message,
+});
+
+
+const normalizedData = normalize(notifications, [notification]);
+
+export default function getAllNotificationsByUser(userId) {
+  const contexts = [];
+  const { notifications: notifEntities, messages } = normalizedData.entities;
+
+  for (const notifId in notifEntities) {
+    const notif = notifEntities[notifId];
+    if (notif.author === userId) {
+      contexts.push(messages[notif.context]);
+    }
+  }
+
+  return contexts;
+}
+
+export { normalizedData };
